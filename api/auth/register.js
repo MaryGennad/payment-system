45b2d00885f07aded27044efe5ab68778302a0d9
MaryGennad <maria-gennadievna@outlook.com>
2026-06-19 14:45:05 +0300

    commit

--- api/auth/register.js
+++ api/auth/register.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: 'Пользователь уже существует' });
     }
 
-    // Хэширование пароля (вручную, так как убрали хук из модели)
+    // Хэширование пароля
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
