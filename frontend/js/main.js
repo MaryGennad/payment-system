e3fa3fea2be345eff819e7b452d6f5f5cfaff0e6
MaryGennad <maria-gennadievna@outlook.com>
2026-06-19 11:13:59 +0300

    API_URL

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,5 @@
 // frontend/js/main.js // fix cache
-const API_BASE = '/api';  // Относительный путь
+const API_URL = '/api';  // Относительный путь
 
 //  Проверка авторизации
 const { token } = window.auth?.getAuth() || {};
