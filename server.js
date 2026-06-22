043db5f6aa69e938e60da241418186cc54c6c440
MaryGennad <maria-gennadievna@outlook.com>
2026-06-22 11:56:12 +0300

    MONGO_URI

--- server.js
+++ server.js
@@ -12,12 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 // Подключение к БД
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
 // Импорт роутов (обрати внимание: добавь .js в конце!)
 import authRoutes from './backend/routes/auth.js';
